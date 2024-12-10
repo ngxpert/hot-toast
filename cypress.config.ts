6ebc588a3b171fd595b7053f84ce8e9bee359214
Dharmen Shah <shhdharmen@gmail.com>
2024-12-10 14:20:45 +0530

    test: fixes for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:4200',
-    excludeSpecPattern: '**/examples/*.spec.js',
+    testIsolation: false,
   },
 });
