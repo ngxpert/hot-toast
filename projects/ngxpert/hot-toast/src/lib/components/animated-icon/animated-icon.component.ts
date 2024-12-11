161b0bc65d0d8f408042907ec026b98200a16d0c
Dharmen Shah <shhdharmen@gmail.com>
2024-12-11 13:18:27 +0530

    fix: allow html in icon

--- projects/ngxpert/hot-toast/src/lib/components/animated-icon/animated-icon.component.ts
+++ projects/ngxpert/hot-toast/src/lib/components/animated-icon/animated-icon.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { IconTheme } from '../../hot-toast.model';
+import { Content, DynamicViewDirective } from '@ngneat/overview';
 
 @Component({
   selector: 'hot-toast-animated-icon',
   templateUrl: './animated-icon.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [DynamicViewDirective],
 })
 export class AnimatedIconComponent {
   @Input() iconTheme: IconTheme;
+  @Input() icon: Content;
 }
+
