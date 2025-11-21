b6ceabb4bf0e119a3b0eef81c4a579b79886931c
Jeevan Mahesha <csejeevanm@gmail.com>
2025-11-21 14:56:39 +0530

    feat: Upgrade Angular to v21, update TypeScript, and implement `provideZoneChangeDetection`.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
@@ -32,5 +32,5 @@ if (!environment.production || window.location.href.includes('test-container')) 
 }
 
 bootstrapApplication(AppComponent, {
-  providers,
+  providers: [provideZoneChangeDetection(), ...providers],
 }).catch((err) => console.error(err));
